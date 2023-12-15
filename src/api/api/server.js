d21bb0da8cdc414a68f9abec24f5b339a24a43c3
Samuel-Jonathan <jtn.samuel@gmail.com>
2023-12-15 11:30:40 +0100

    Génération du token

--- src/api/api/server.js
+++ src/api/api/server.js
@@ -55,10 +55,61 @@ app.route(prefix + '/assignments')
   .post(assignment.postAssignment)
   .put(assignment.updateAssignment);
 
+  // Configurer l'authentification
+const User = require('./model/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+// Inscription
+app.post('/api/register', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = new User({
+      username: req.body.username,
+      password: hashedPassword
+    });
+    await user.save();
+    res.status(201).send('User registered successfully');
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// Connexion
+app.post('/api/login', async (req, res) => {
+  const user = await User.findOne({ username: req.body.username });
+  if (!user) return res.status(404).send('User not found');
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(401).send('Invalid password');
+
+  const token = jwt.sign({ userId: user._id }, 'your-secret-key', { expiresIn: '1h' });
+  res.status(200).json({ token });
+});
+
+// Exemple d'utilisation du token dans une route sécurisée
+app.get('/api/secure-route', authenticateToken, (req, res) => {
+  // ... code de la route sécurisée
+});
+
+function authenticateToken(req, res, next) {
+  const token = req.headers['authorization'];
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, 'your-secret-key', (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+
 // On démarre le serveur
 app.listen(port, "0.0.0.0");
 console.log('Serveur démarré sur http://localhost:' + port);
 
 module.exports = app;
+
+
 
 
