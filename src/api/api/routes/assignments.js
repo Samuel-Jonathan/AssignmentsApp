27a13833f8931847bfc87ec8ac6ff346d0f86cd9
Samuel-Jonathan <jtn.samuel@gmail.com>
2024-01-04 20:24:06 +0100

    Fix for get the student name

--- src/api/api/routes/assignments.js
+++ src/api/api/routes/assignments.js
@@ -1,44 +1,51 @@
 let Assignment = require('../model/assignment');
 
+
 function getAssignments(req, res) {
     const options = {
-        page: parseInt(req.query.page) || 1,
-        limit: parseInt(req.query.limit) || 10,
+        page: parseInt(req.query.page, 10) || 1,
+        limit: parseInt(req.query.limit, 10) || 10,
     };
 
-    Assignment.aggregatePaginate(
-        [
-            {
-                $lookup: {
-                    from: 'students', 
-                    localField: 'student', 
-                    foreignField: 'studentId',
-                    as: 'student' 
-                }
-            },
-            {
-                $unwind: '$student' 
-            },
-            {
-                $project: {
-                    _id: 1,
-                    studentName: { $concat: ['$student.first_name', ' ', '$student.last_name'] },
-                    dateDeRendu: 1,
-                    nom: 1,
-                    rendu: 1,
-                    subject: 1,
-                    studentId: 1
-                }
+    Assignment.aggregate([
+        {
+            $lookup: {
+                from: 'students', // Assurez-vous que c'est le nom correct de la collection des étudiants dans MongoDB
+                localField: 'studentId', // Le champ dans 'assignments' qui contient la référence à 'students'
+                foreignField: 'id', // Le champ correspondant dans 'students', généralement '_id'
+                as: 'studentDetails' // Le résultat de la jointure sera stocké dans ce nouveau champ
             }
-        ],
-        options,
-        (err, assignments) => {
-            if (err) {
-                res.send(err)
+        },
+        {
+            $unwind: {
+                path: '$studentDetails',
+                preserveNullAndEmptyArrays: true // Pour garder les assignments qui n'ont pas d'étudiants liés
             }
-            res.send(assignments);
+        },
+        {
+            $project: {
+                _id: 1,
+                id: 1,
+                studentName: {
+                    $concat: [
+                        '$studentDetails.first_name', ' ', '$studentDetails.last_name'
+                    ]
+                },
+                dateDeRendu: 1,
+                nom: 1,
+                rendu: 1,
+                subject: 1,
+                studentId: 1
+            }
         }
-    );
+    ])
+    .then(assignments => {
+        res.send(assignments);
+    })
+    .catch(err => {
+        console.error('Error while getting assignments:', err);
+        res.status(500).send({ message: 'Error while getting assignments', error: err });
+    });
 }
 
   
