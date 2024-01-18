0cbf04d5c8c2c0b185247146f1845456b3a22f97
Samuel-Jonathan <jtn.samuel@gmail.com>
2024-01-18 01:12:57 +0100

    Sort assignments

--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -1,19 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Assignment } from './assignment.model';
 import { AssignmentsService } from '../shared/assignments.service';
+import { MatSort, MatSortHeader } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-assignments',
   templateUrl: './assignments.component.html',
   styleUrls: ['./assignments.component.css']
 })
+
 export class AssignmentsComponent implements OnInit {
+  @ViewChild(MatSort) sort!: MatSort;
+ 
 
   titre: String = "Mon application Angular sur les assignments"
   ajoutActive = true;
   assignmentSelectionne: Assignment | null = null;
   formVisible = false;
-  assignments!: Assignment[];
+  assignments!: MatTableDataSource<Assignment>;
 
   page: number = 1;
   limit: number = 10;
@@ -32,18 +37,20 @@ export class AssignmentsComponent implements OnInit {
       this.ajoutActive = true;
     }, 2000)
   }
-
+  ascendingOrder = true;
   getAssignments() {
     this.assignmentService.getAssignmentPagine(this.page, this.limit)
       .subscribe(
         data => {
-          this.assignments = data.docs;
+          this.assignments = new MatTableDataSource(data.docs);
           this.totalDocs = data.totalDocs;
           this.totalPages = data.totalPages;
           this.nextPage = data.nextPage;
           this.prevPage = data.prevPage;
           this.hasPrevPage = data.hasPrevPage;
           this.hasNextPage = data.hasNextPage;
+          this.assignments.sort = this.sort;
+          this.sort.disableClear = true;
         }
       )
   }
